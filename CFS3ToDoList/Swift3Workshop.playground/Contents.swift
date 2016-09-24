f1c9035afcbe34ae79baca91e633b4dbc0c35379
Lex Myers <lex.myers@gmail.com>
2016-09-24 13:05:37 -0700

    Second commit

--- CFS3ToDoList/Swift3Workshop.playground/Contents.swift
+++ CFS3ToDoList/Swift3Workshop.playground/Contents.swift
@@ -127,6 +127,29 @@ case 100..<1000:
 default:
     countDescription = "unknown"
 }
+
+let somePoint = (x: 1,y: 1)
+
+switch somePoint {
+case (0, 0):
+    print("(0, 0) is at the origin")
+case (_, 0):
+    print ("(\(somePoint.0), 0) is on the x-axis")
+case (0, _):
+    print("0, \(somePoint.1)) is on the y-axis")
+case (-2...2, -2...2):
+    print(("(\(somePoint.0), \(somePoint.1)) is inside the box"))
+default:
+    print("(\(somePoint.0), \(somePoint.1)) is outside of the box")
+}
+
 //Optionals
 
+var surveyAnswer: String?
+
+surveyAnswer = "This was so much fun!!!"
+
+if surveyAnswer != nil{
+    print("SurveyAnswer is not nil.")
+}
 
